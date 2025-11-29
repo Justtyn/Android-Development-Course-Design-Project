c601a1f171ff423867b72c923beae5872ce8a216
JUSTYN <94769841+Justtyn@users.noreply.github.com>
2025-11-29 23:10:10 +0800

    添加猫咪百科页面

--- Meow/app/src/main/java/com/justyn/meow/cat/CatWikiActivity.java
+++ Meow/app/src/main/java/com/justyn/meow/cat/CatWikiActivity.java
@@ -1,6 +1,8 @@
 package com.justyn.meow.cat;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,16 @@ import com.justyn.meow.R;
 
 public class CatWikiActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cat_wiki);
+
+        WebView webView = findViewById(R.id.webViewMeow);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/cat_wiki.html");
+
     }
 }
